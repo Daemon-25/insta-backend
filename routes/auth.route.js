49c0e63d8b22c7fb135042a62cb1c3a3b996ef12
daemon25 <lakshayrose0000@gmail.com>
2022-07-16 00:43:03 +0530

    Final Changes

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -7,9 +7,6 @@ module.exports = (app) => {
 	// Route to handle SignIn requests
 	app.post("/signin", controller.signin);
 
-	// Route to handle Reset Passwords requests
-	app.post("/reset-pwd", controller.resetPwd);
-
 	// Route to handle Create New Passwords requests
 	app.post("/new-pwd", controller.newPwd);
 };
